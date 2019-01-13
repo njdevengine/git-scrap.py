392db4f7253df3a9b408fd7fdd82042a2ad5deb9
njdevengine <newjerseydevelopment@gmail.com>
2019-01-12 23:16:42 -0500

    Update githubscraper.py

--- githubscraper.py
+++ githubscraper.py
@@ -21,19 +21,17 @@ for i in range(1,55):
     urls.append('https://github.com/orgs/RutgersCodingBootcamp/people?page='+ str(i) +'&query=&utf8=')
 
 soup_array = []
+soup_array = []
+n=0
+my_url = urls[n]
 def get_soup():
-    soup_array = []
-    n=0
-    my_url = urls[n]
-    while n < 56:
+    while n <56:
         uReq(my_url)
         uClient = uReq(my_url)
         page_html = uClient.read()
         uClient.close()
         page_soup = str(soup(page_html, "html.parser"))
         soup_array.append(page_soup)
-        n + 1
-        time.sleep(10)
-        
-get_soup()
-
+        n = n+1
+        print('saving soup number '+str(n))
+        time.sleep(2)
